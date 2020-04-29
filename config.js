b92403a90ba8bbe010c2f3ffccc3d2b8d7d23091
Min Jun Kim <mk7pe@virginia.edu>
2020-04-29 15:44:54 -0400

    Update configuration and about page

--- config.js
+++ config.js
@@ -1,43 +1,43 @@
-"use strict";
+'use strict'
 
 module.exports = {
-  url: "https://mnjn.me",
-  pathPrefix: "/",
-  title: "MNJN",
-  subtitle: "The personal website and blog of Min Jun Kim",
-  copyright: "© Min Jun Kim 2019.",
-  disqusShortname: "",
+  url: 'https://mnjn.me',
+  pathPrefix: '/',
+  title: 'MNJN',
+  subtitle: 'The personal website and blog of Min Jun Kim',
+  copyright: '© Min Jun Kim 2020.',
+  disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: "UA-112603320-4",
+  googleAnalyticsId: 'UA-112603320-4',
   useKatex: false,
   menu: [
     {
-      label: "Blog",
-      path: "/"
+      label: 'Blog',
+      path: '/',
     },
     {
-      label: "About",
-      path: "/pages/about"
-    }
+      label: 'About',
+      path: '/pages/about',
+    },
   ],
   author: {
-    name: "Min Jun Kim",
-    photo: "/social-image.png",
+    name: 'Min Jun Kim',
+    photo: '/social-image.png',
     bio:
-      "UVA '21. I like to write words and code. Passionate about quality experiences.",
+      "UVA '21. I like to write words and code. Passionate about engineering and space exploration.",
     contacts: {
-      email: "hello@mnjn.me",
-      twitter: "itsmnjn",
-      telegram: "",
-      facebook: "",
-      github: "itsmnjn",
-      rss: "",
-      vkontakte: "",
-      linkedin: "",
-      instagram: "",
-      line: "",
-      gitlab: "mnjn",
-      weibo: ""
-    }
-  }
-};
+      email: 'mk7pe@virginia.edu',
+      twitter: 'itsmnjn',
+      telegram: '',
+      facebook: '',
+      github: 'itsmnjn',
+      rss: '',
+      vkontakte: '',
+      linkedin: '',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+    },
+  },
+}
--- content/pages/about.md
+++ content/pages/about.md
@@ -1,11 +1,15 @@
 ---
-title: "About"
-template: "page"
-socialImage: ""
+title: 'About'
+template: 'page'
+socialImage: ''
 ---
 
 Hi, I'm Min Jun. I do go by Eric, but feel free to call me whichever.
 
-I'm currently a second-year studying Computer Engineering at the [University of Virginia](https://www.virginia.edu), planning to graduate in December 2021.
+I'm a third-year studying Computer Science at the [University of Virginia](https://www.virginia.edu), planning to graduate in December 2021.
 
-I am primarily interested in full-stack web development, a little bit of mobile, and am quite excited for the coming AR revolution 🙂
+I'm quite passionate about a lot of things, but primarily engineering/building stuff (anything, really, but I lean towards software due to easy accessibility). My other interests lie in space (particularly the potential for interstellar exploration), music (Mac Miller's posthumous album is simply grand—rest in peace, Mac), and writing (fiction, poetry, and this blog!).
+
+In software, I enjoy working with the whole stack, completely language agnostic, from web applications to embedded systems to artificial intelligence and data science.
+
+My current aspiration is to work as a Software Engineer at SpaceX in their Vehicle department.
